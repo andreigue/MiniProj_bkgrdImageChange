d9d209b4a914ef091c749b7a38fd2adbe40e14c3
andreigue <andreiguevorkian@mail.mcgill.ca>
2020-01-16 18:54:32 -0500

    able to download image, issue withfile path

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ and https://www.dataquest.io/blog/web-scraping-tutorial-python/
 import requests #gives acces to websites
 from bs4 import BeautifulSoup  #bs gives nice structures to work with when scraping (understands html/css)
 import ctypes  #allow to access user32.dll
-
+import urllib.request #to download img
+import os
 import webbrowser
 
 url = "https://apod.nasa.gov/"
@@ -19,11 +20,13 @@ def changeWallpaper(imagePath):
     
 page = requests.get(url)        #makes a GET request to the web server hosting the website
 soup=BeautifulSoup(page.content, 'html.parser')
-date = soup.findAll("p")[1].text.strip()
+#date = soup.findAll("p")[1].text.strip()
 image = soup.find("img")
 url += image["src"]
 print(url)
 webbrowser.open(url)
-#changeWallpaper()
-    
-    
+
+urllib.request.urlretrieve(url, "img_of_the_day.jpg")
+pathToImg = os.path.normpath('C:\Users\PC\Desktop\gitProjects\personalAutomationMiniProjects\bkgrdPictureChange\img_of_the_day')
+#pathToImg = 'C:\Users\PC\Desktop\gitProjects\personalAutomationMiniProjects\bkgrdPictureChange\img_of_the_day'
+changeWallpaper(pathToImg)    
