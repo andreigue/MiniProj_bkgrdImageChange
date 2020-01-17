e3300d27e52f586adbc97f9ec3549ab7606233c5
andreigue <andreiguevorkian@mail.mcgill.ca>
2020-01-16 19:04:21 -0500

    fixed file path, but getting dark backgroun

--- main.py
+++ main.py
@@ -9,24 +9,26 @@ import requests #gives acces to websites
 from bs4 import BeautifulSoup  #bs gives nice structures to work with when scraping (understands html/css)
 import ctypes  #allow to access user32.dll
 import urllib.request #to download img
-import os
+from os import path
 import webbrowser
 
 url = "https://apod.nasa.gov/"
 
 def changeWallpaper(imagePath):
     SPI_SETDESKWALLPAPER = 20 
-    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, newImage , 0)
+    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, imagePath, 0)
     
 page = requests.get(url)        #makes a GET request to the web server hosting the website
 soup=BeautifulSoup(page.content, 'html.parser')
-#date = soup.findAll("p")[1].text.strip()
+date = soup.findAll("p")[1].text.strip()
 image = soup.find("img")
 url += image["src"]
-print(url)
-webbrowser.open(url)
+#urllib.request.urlretrieve(url, f"image {date}.jpg")       #download img 
+urllib.request.urlretrieve(url, "img.jpg")
+#print(url)
+#webbrowser.open(url)   #open in web browser
+changeWallpaper(path.abspath("img.jpg"))
 
-urllib.request.urlretrieve(url, "img_of_the_day.jpg")
-pathToImg = os.path.normpath('C:\Users\PC\Desktop\gitProjects\personalAutomationMiniProjects\bkgrdPictureChange\img_of_the_day')
+#pathToImg = os.path.normpath('C:\Users\PC\Desktop\gitProjects\personalAutomationMiniProjects\bkgrdPictureChange\img_of_the_day')
 #pathToImg = 'C:\Users\PC\Desktop\gitProjects\personalAutomationMiniProjects\bkgrdPictureChange\img_of_the_day'
-changeWallpaper(pathToImg)    
+#changeWallpaper(pathToImg)    
